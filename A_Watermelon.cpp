438ea3ed13873c5964037e0734f269baf2fa3732
Ashmit <codeatashmit@gmail.com>
2024-07-29 22:26:13 +0530

    codeforces

--- A_Watermelon.cpp
+++ A_Watermelon.cpp
@@ -1,22 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 bool find(int n){
-    if(n%2==0 &&  n>2){
-        return true;
-    }
-    else{
-        return false;
-    }
+    if(n%2==0 &&  n>2){return true;}
+    else{return false;}
 }
 
-int main(){
-    int n;
-    cin >> n;
+int main(){int n;cin >> n;
     bool ans = find(n);
-    if(ans==true){
-        cout << "YES" <<"\n";
-    }
-    else{
-        cout << "NO" << "\n";
-    }
+    if(ans==true){cout << "YES" <<"\n";}
+    else{cout << "NO" << "\n";}
 }
--- A_Way_Too_Long_Words.cpp
+++ A_Way_Too_Long_Words.cpp
@@ -4,27 +4,16 @@ using namespace std;
 string solve(string s){
     int n = s.size();
     if(n>10){
-        string ans="";
-        ans+=s[0];
-        string count = to_string(n-2);
-        ans+=count;
-        ans+=s[n-1];
-        return ans;
-    }
-    else{
-        return s;
-    }
+        string ans="";ans+=s[0];
+        string count = to_string(n-2);ans+=count;
+        ans+=s[n-1];return ans;}
+    else{return s;}
 }
 void solution(int test){
     while(test--){
-        string s;
-        cin >> s;
-        string ans = solve(s);
-        cout << ans << "\n";
-    }
+        string s;cin >> s;
+        string ans = solve(s);cout << ans << "\n";}
 }
 int main(){
-    int test;
-    cin >> test;
-    solution(test);
+    int test;cin >> test;solution(test);
 }
